dcd84fa6ab7ca542e0ba60e9e7d30844eed3122a
juleimisf <juleimis1314@gmail.com>
2024-06-05 15:24:36 -0400

    Initial commit

---
+++ formValidationLibrary/src/main/java/com/example/formvalidationlibrary/Validators.kt
@@ -0,0 +1,16 @@
+package com.example.formvalidationlibrary
+
+object Validators {
+    fun isValidEmail(email: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    fun isValidPhone(phone: String): Boolean {
+        return android.util.Patterns.PHONE.matcher(phone).matches() && phone.length >= 10
+    }
+
+    fun isValidPassword(password: String): Boolean {
+        val passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$"
+        return password.matches(passwordPattern.toRegex())
+    }
+}
